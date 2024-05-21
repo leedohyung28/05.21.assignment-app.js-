1394f91dc90ff522e36575b75f8c97d324a85a8b
leedohyung28 <dhleee0123@gmail.com>
2024-05-21 21:06:53 +0900

    05.21 app.js
    5월 21일 app.js 구현 과제

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.listen(process.env.PORT);
+
+const userRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+const likesRouter = require("./routes/likes");
+const ordersRouter = require("./routes/orders");
+const cartRouter = require("./routes/cart");
+
+app.use("/", userRouter);
+app.use("/books", booksRouter);
+app.use("/likes", likesRouter);
+app.use("/orders", ordersRouter);
+app.use("/cart", cartRouter);
